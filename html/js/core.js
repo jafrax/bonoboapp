e6c0748d0e248ab9ebd5b288a4a75a639b15db65
Chavid <fatoni.visione@gmail.com>
2015-06-17 10:37:12 +0700

    s

--- html/js/core.js
+++ html/js/core.js
@@ -15,5 +15,16 @@ $(document).ready(function() {
 	/*MODAL*/
 	$('.modal-trigger').leanModal();
 	/*END MODAL*/
+
+	/*TAB*/
+	$('ul.tabs').tabs('select_tab', 'tab_id');
+	/*END TAB*/
+
+	/*DATE PICKER*/
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	});
+	/*END DATE PICKER*/
 	
 });
