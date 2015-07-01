a5b5d75a65118cb82dfaa5256a1031d88e7da166
Chavid <fatoni.visione@gmail.com>
2015-07-01 08:54:44 +0700

    s

--- application/views/enduser/toko/bg_step_6.php
+++ application/views/enduser/toko/bg_step_6.php
@@ -26,7 +26,7 @@ echo"
 												Nama Bank								
 											</div>
 											<div class='input-field col s12 m12'>																
-												<select>
+												<select class='chosen-select'>
 													<option value='' disabled selected>Choose your option</option>
 													<option value='1'>Option 1</option>
 													<option value='2'>Option 2</option>
