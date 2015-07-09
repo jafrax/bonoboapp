7feb609eab9815ac0be07079a38ce86e709cf4fa
Dinar Wahyu Wibowo <v1.imcare@gmail.com>
2015-07-09 09:58:09 +0700

    Signed-off-by: Dinar Wahyu Wibowo <v1.imcare@gmail.com>

--- application/models/enduser/model_preorder.php
+++ application/models/enduser/model_preorder.php
@@ -17,4 +17,12 @@ class Model_preorder extends CI_Model
 						->get('tb_product p');
 	}
 	
+	function get_belum_selesai($id){
+		return $this->db->where('i.status_pre_order',0)
+						->where('p.id',$id)
+						->join('tb_invoice_product ip','ip.invoice_id=i.id')
+						->join('tb_product_varian v','v.id=ip.product_varian_id')
+						->join('tb_product p','p.id=v.product_id')
+						->get('tb_invoice i');
+	}
 }
--- application/views/enduser/preorder/bg_preorder.php
+++ application/views/enduser/preorder/bg_preorder.php
@@ -6,6 +6,7 @@ echo "
 							<div class='col s12 m12'>
 								<ul class='row formbody'>";
 								foreach ($produk->result() as $row) {
+									$count = $this->model_preorder->get_belum_selesai($row->id)->num_rows();
 									echo "
 									<li class='col s12 m6 listanggodaf'>
 										<div class='col s3 m5 l4'>";
@@ -19,10 +20,10 @@ echo "
 										<div class='col s9 m7 l8'>
 											<p class=' blue-grey-text lighten-3 right'>19.09</p>
 											<p><a href=''><b class='userangoota'>".$row->name."</b></a></p>				
-											<span class='red-text'>20 Belum Selesai</span>										
+											<span class='red-text'>$count Belum Selesai</span>										
 											<p>
-											<a href='#' class='btn-flat waves-effect waves-light teal white-text right'>Selesai Semua</a> 
-											<a href='pesanan_pre_order_detail.html' class='btn-flat waves-effect waves-light deep-orange white-text right'>Lihat Pesanan</a></p>
+											<a onclick=javascript:all_done() class='btn-flat waves-effect waves-light teal white-text right'>Selesai Semua</a> 
+											<a href='".base_url()."preorder/detail/".base64_encode($row->id)."' class='btn-flat waves-effect waves-light deep-orange white-text right'>Lihat Pesanan</a></p>
 										</div>
 									</li>";
 								}
