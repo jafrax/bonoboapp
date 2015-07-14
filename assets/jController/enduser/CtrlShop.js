24f1596155e17b64bc5a44e534f963fa0cb6cff3
Dinar Wahyu Wibowo <v1.imcare@gmail.com>
2015-07-14 13:11:21 +0700

    Signed-off-by: Dinar Wahyu Wibowo <v1.imcare@gmail.com>

--- assets/jController/enduser/CtrlShop.js
+++ assets/jController/enduser/CtrlShop.js
@@ -71,7 +71,7 @@ function CtrlShopStep1(){
 			var div = document.createElement("div");
 			
 			div.innerHTML = "<div class='col s12 m3' id='kontak"+sequence+"'>Nama kontak</div><div class='col s12 m5'><input  name='txtAttributeId"+sequence+"' type='hidden' value=''><input id='txtAttributeId"+sequence+"' name='txtAttributeName"+sequence+"' placeholder='BBM/whatsapp/Line' type='text' class='validate'></div><div class='col s12 m3'>Pin/ID/Nomor</div><div class='col s12 m5'><input name='txtAttributeValue"+sequence+"' type='text' placeholder='Ex : AD9876/bonoboLine' class='validate'></div><div class='col s12 m5'><a href='#delete_kontak_"+sequence+"'  class='modal-trigger btn-floating btn-xs waves-effect waves-light red right' onclick=CtrlShopStep1.deletestep1("+sequence+");><i class='mdi-navigation-close'></i></a></div>";
-			div.setAttribute("class","row valign-wrapper counter");
+			div.setAttribute("class","row valign-wrapper counter attr-"+sequence);
 			divAttributes.append(div);
 			intAttributeCount.value = sequence;
 			if ($('.counter').length > 2 ) {
@@ -85,24 +85,24 @@ function CtrlShopStep1(){
 		
 	}
 	
-	function deletestep1(e){
+	function deletestep1(e,a){
 		var txtAttributeId = $hs("txtAttributeId"+e);
 		var divKontak = $("#divKontak"+e);
 		
 		if(txtAttributeId.value == ""){
 			divKontak.slideUp("slow").remove();
 			if ($('.counter').length < 3  ) {			
-				$('#tombol-tambah').show();
+				$('#aAttributeAdd').show();
 			}
 		}else{
 			$.ajax({
 				type: 'POST',
 				data: "id="+txtAttributeId.value,
-				url: base_url+'toko/doStep1deletekontak/',
+				url: base_url+'toko/dostep1deletekontak/',
 				success: function(result) {
 					var response = JSON.parse(result);
 					if(response.result == 1){
-						divKontak.slideUp("slow");
+						divKontak.slideUp("slow").remove();
 					}else{
 						$hs_notif("#notifStep5",response.message);
 					}
