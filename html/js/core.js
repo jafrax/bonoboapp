3b41de1c95406799b8737832ac6654a26348aed4
zaidrh <zaid.r@visione-system.com>
2015-07-15 13:18:23 +0700

    Signed-off-by: zaidrh <zaid.r@visione-system.com>

--- html/js/core.js
+++ html/js/core.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 	var config = {
       '.chosen-select'           : {},
       '.chosen-select-deselect'  : {allow_single_deselect:true},
-      '.chosen-select-no-single' : {disable_search_threshold:10},
+      '.chosen-select-no-single' : {disable_search_threshold:5},
       '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
       '.chosen-select-width'     : {width:"95%"}
     }
