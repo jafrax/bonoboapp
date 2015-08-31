ea88ac9b7d91033494415c465b8e0cb3ac3c7f35
Bayu <hsbn89@yahoo.co.id>
2015-08-31 15:54:28 +0700

    nambah live chat

--- application/views/enduser/template/bg_header.php
+++ application/views/enduser/template/bg_header.php
@@ -54,16 +54,16 @@ echo"
 ?>
 
 <script type='text/javascript'>
-		var $_Tawk_API={},$_Tawk_LoadStart=new Date();
-		(function(){
-			var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0];
-			s1.async=true;
-			s1.src='https://embed.tawk.to/55be11445355f0e91c613ea6/default';
-			s1.charset='UTF-8';
-			s1.setAttribute('crossorigin','*');
-			s0.parentNode.insertBefore(s1,s0);
-		})();
-	</script>
+	var $_Tawk_API={},$_Tawk_LoadStart=new Date();
+	(function(){
+		var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/55be11445355f0e91c613ea6/default';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+	})();
+</script>
 
 
 <?php
