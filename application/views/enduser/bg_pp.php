ef4835ec2e8b8b5e49c442edda1a6c2a05279c9d
Chavid <fatoni.visione@gmail.com>
2015-09-15 10:33:33 +0700

    d

--- application/views/enduser/bg_pp.php
+++ application/views/enduser/bg_pp.php
@@ -67,10 +67,10 @@ echo "
 
 						<div class='row formbody'>
 							<p>
-								<p class='light'>Adanya Kebijakan Privasi ini adalah komitmen nyata dari Bonobo untuk menghargai dan melindungi setiap informasi pribadi Pengguna situs www.Bonobo.com (situs Bonobo).</p>
-								<p class='light'>Kebijakan ini (beserta syarat-syarat penggunaan dari situs Bonobo sebagaimana tercantum dalam \"Syarat & Ketentuan\" dan dokumen lain yang tercantum di situs Bonobo) menetapkan dasar atas segala data pribadi yang Pengguna berikan kepada Bonobo atau yang Bonobo kumpulkan dari Pengguna, kemudian akan diproses oleh Bonobo mulai pada saat melakukan pendaftaran, mengakses dan menggunakan layanan Bonobo. Harap memperhatikan ketentuan di bawah ini secara seksama untuk memahami pandangan dan kebiasaan Bonobo berkenaan dengan data Bonobo memperlakukan informasi tersebut.</p>
-								<p class='light'>Dengan mengakses dan menggunakan layanan situs Bonobo, Pengguna dianggap telah membaca, memahami dan memberikan persetujuannya terhadap pengumpulan dan penggunaan data pribadi Pengguna sebagaimana diuraikan di bawah ini.</p>
-								<p class='light'>Kebijakan Privasi ini mungkin di ubah dan/atau diperbaharui dari waktu ke waktu tanpa pemberitahuan sebelumnya. Bonobo menyarankan agar anda membaca secara seksama dan memeriksa halaman Kebijakan Privasi ini dari waktu ke waktu untuk mengetahui perubahan apapun. Dengan tetap mengakses dan menggunakan layanan Bonobo, maka pengguna dianggap menyetujui perubahan-perubahan dalam Kebijakan Privasi ini.</p>
+								<p>Adanya Kebijakan Privasi ini adalah komitmen nyata dari Bonobo untuk menghargai dan melindungi setiap informasi pribadi Pengguna situs www.Bonobo.com (situs Bonobo).</p>
+								<p>Kebijakan ini (beserta syarat-syarat penggunaan dari situs Bonobo sebagaimana tercantum dalam \"Syarat & Ketentuan\" dan dokumen lain yang tercantum di situs Bonobo) menetapkan dasar atas segala data pribadi yang Pengguna berikan kepada Bonobo atau yang Bonobo kumpulkan dari Pengguna, kemudian akan diproses oleh Bonobo mulai pada saat melakukan pendaftaran, mengakses dan menggunakan layanan Bonobo. Harap memperhatikan ketentuan di bawah ini secara seksama untuk memahami pandangan dan kebiasaan Bonobo berkenaan dengan data Bonobo memperlakukan informasi tersebut.</p>
+								<p>Dengan mengakses dan menggunakan layanan situs Bonobo, Pengguna dianggap telah membaca, memahami dan memberikan persetujuannya terhadap pengumpulan dan penggunaan data pribadi Pengguna sebagaimana diuraikan di bawah ini.</p>
+								<p>Kebijakan Privasi ini mungkin di ubah dan/atau diperbaharui dari waktu ke waktu tanpa pemberitahuan sebelumnya. Bonobo menyarankan agar anda membaca secara seksama dan memeriksa halaman Kebijakan Privasi ini dari waktu ke waktu untuk mengetahui perubahan apapun. Dengan tetap mengakses dan menggunakan layanan Bonobo, maka pengguna dianggap menyetujui perubahan-perubahan dalam Kebijakan Privasi ini.</p>
 								<p class='light'>
 								<ul>
 								<li><a href='#informasi'>Informasi Pengguna</a></li>
@@ -81,49 +81,52 @@ echo "
 								</ul>
 								</p>
 								________________________________________
-								<h3 id='informasi' class='light title-faq '>Informasi Pengguna</h3>
-								<p class='light'>Bonobo mengumpulkan informasi Pengguna dengan tujuan untuk memperoses dan memperlancar proses penggunaan situs Bonobo. Tindakan tersebut telah memperoleh persetujuan Pengguna pada saat pengumpulan informasi.</p><br>
-								<p class='light'>Bonobo mengumpulkan informasi pribadi sewaktu Pengguna mendaftar ke Bonobo, sewaktu Pengguna menggunakan layanan Bonobo, sewaktu Pengguna mengunjungi halaman Bonobo atau halaman-halaman para mitra tertentu dari Bonobo, dan sewaktu Anda menghubungi Bonobo. Bonobo dapat menggabung informasi mengenai Pengguna yang kami miliki dengan informasi yang kami dapatkan dari para mitra usaha atau perusahaan-perusahaan lain.</p><br>
-								<p class='light'>Sewaktu Anda mendaftar untuk menjadi Pengguna Bonobo, maka Bonobo akan mengumpulkan data pribadi Pengguna, yakni nama lengkap, alamat e-mail, nomor telepon yang dapat dihubungi, password dan informasi lainnya yang diperlukan. Dengan memberikan informasi / data tersebut, Anda memahami, bahwa Bonobo dapat meminta dan mendapatkan setiap informasi / data pribadi Pengguna untuk kesinambungan dan keamanan situs ini.</p><br>
-								<p class='light'>Bonobo akan mengumpulkan dan mengolah keterangan lengkap mengenai transaksi atau penawaran atau hubungan financial lainnya yang Pengguna laksanakan melalui situs Bonobo dan mengenai pemenuhan pesanan Pengguna.</p><br>
-								<p class='light'>Bonobo akan mengumpulkan dan mengolah data mengenai kunjungan Pengguna ke situs Bonobo, termasuk namun tidak terbatas pada data lalu-lintas, data lokasi, weblog, tautan ataupun data komunikasi lainnya, apakah hal tersebut disyaratkan untuk tujuan penagihan atau lainnya, serta sumberdaya yang Pengguna akses.</p><br>
-								<p class='light'>Pada saat Pengguna menghubungi Bonobo, maka Bonobo menyimpan catatan mengenai korespondensi tersebut dan isi dari komunikasi antara Pengguna dan Bonobo.</p><br>
-								<p class='light'>Pengguna memahami dan menyetujui bahwa nama Pengguna dan daerah Kota (bukan alamat lengkap) Pengguna merupakan informasi umum yang tertera di halaman profile Bonobo Pengguna.</p><br>
-								<p class='light'>Bonobo dengan sendirinya menerima dan mencatat informasi dari komputer dan browser Pengguna, termasuk alamat IP, informasi cookie Bonobo, atribut piranti lunak dan piranti keras, serta halaman yang Pengguna minta.</p><br>
-								<p class='light'>Setiap informasi / data Pengguna yang disampaikan kepada Bonobo dan/atau yang dikumpulkan oleh Bonobo dilindungi dengan upaya sebaik mungkin oleh perangkat keamanan teruji, yang digunakan oleh Bonobo secara elektronik. Meskipun demikian, Bonobo tidak menjamin kerahasiaan informasi yang Pengguna sampaikan tersebut, dalam kondisi adanya pihak-pihak lain yang mengambil atau mempergunakan informasi Pengguna dengan melawan hukum serta tanpa izin Bonobo.</p><br>
-								<p class='light'>Kerahasiaan kata sandi atau password merupakan tanggung jawab masing-masing Pengguna. Bonobo tidak bertanggung jawab atas kerugian yang dapat ditimbulkan akibat kelalaian pengguna dalam menjaga kerahasiaan passwordnya.</p><br>
+								<h3 id='informasi' class='light title-faq '>Informasi Pengguna</h3> <br>
+								<p>Bonobo mengumpulkan informasi Pengguna dengan tujuan untuk memperoses dan memperlancar proses penggunaan situs Bonobo. Tindakan tersebut telah memperoleh persetujuan Pengguna pada saat pengumpulan informasi.</p>
+								<p>Bonobo mengumpulkan informasi pribadi sewaktu Pengguna mendaftar ke Bonobo, sewaktu Pengguna menggunakan layanan Bonobo, sewaktu Pengguna mengunjungi halaman Bonobo atau halaman-halaman para mitra tertentu dari Bonobo, dan sewaktu Anda menghubungi Bonobo. Bonobo dapat menggabung informasi mengenai Pengguna yang kami miliki dengan informasi yang kami dapatkan dari para mitra usaha atau perusahaan-perusahaan lain.</p>
+								<p>Sewaktu Anda mendaftar untuk menjadi Pengguna Bonobo, maka Bonobo akan mengumpulkan data pribadi Pengguna, yakni nama lengkap, alamat e-mail, nomor telepon yang dapat dihubungi, password dan informasi lainnya yang diperlukan. Dengan memberikan informasi / data tersebut, Anda memahami, bahwa Bonobo dapat meminta dan mendapatkan setiap informasi / data pribadi Pengguna untuk kesinambungan dan keamanan situs ini.</p>
+								<p>Bonobo akan mengumpulkan dan mengolah keterangan lengkap mengenai transaksi atau penawaran atau hubungan financial lainnya yang Pengguna laksanakan melalui situs Bonobo dan mengenai pemenuhan pesanan Pengguna.</p>
+								<p>Bonobo akan mengumpulkan dan mengolah data mengenai kunjungan Pengguna ke situs Bonobo, termasuk namun tidak terbatas pada data lalu-lintas, data lokasi, weblog, tautan ataupun data komunikasi lainnya, apakah hal tersebut disyaratkan untuk tujuan penagihan atau lainnya, serta sumberdaya yang Pengguna akses.</p>
+								<p>Pada saat Pengguna menghubungi Bonobo, maka Bonobo menyimpan catatan mengenai korespondensi tersebut dan isi dari komunikasi antara Pengguna dan Bonobo.</p>
+								<p>Pengguna memahami dan menyetujui bahwa nama Pengguna dan daerah Kota (bukan alamat lengkap) Pengguna merupakan informasi umum yang tertera di halaman profile Bonobo Pengguna.</p>
+								<p>Bonobo dengan sendirinya menerima dan mencatat informasi dari komputer dan browser Pengguna, termasuk alamat IP, informasi cookie Bonobo, atribut piranti lunak dan piranti keras, serta halaman yang Pengguna minta.</p>
+								<p>Setiap informasi / data Pengguna yang disampaikan kepada Bonobo dan/atau yang dikumpulkan oleh Bonobo dilindungi dengan upaya sebaik mungkin oleh perangkat keamanan teruji, yang digunakan oleh Bonobo secara elektronik. Meskipun demikian, Bonobo tidak menjamin kerahasiaan informasi yang Pengguna sampaikan tersebut, dalam kondisi adanya pihak-pihak lain yang mengambil atau mempergunakan informasi Pengguna dengan melawan hukum serta tanpa izin Bonobo.</p>
+								<p>Kerahasiaan kata sandi atau password merupakan tanggung jawab masing-masing Pengguna. Bonobo tidak bertanggung jawab atas kerugian yang dapat ditimbulkan akibat kelalaian pengguna dalam menjaga kerahasiaan passwordnya.</p>
 						<a href='#top'>kembali ke atas</a><br>
 								________________________________________
-								<h3 id='penggunaan' class='light title-faq '>Penggunaan Informasi</h3>
-								<p class='light'>Bonobo dapat menggunakan keseluruhan informasi / data Pengguna sebagai acuan untuk upaya peningkatan produk dan pelayanan.</p><br>
-								<p class='light'>Bonobo dapat mempergunakan dan mengolah Informasi / data Pengguna dengan tujuan untuk menyesuaikan situs Bonobo sesuai dengan minat Pengguna.</p><br>
-								<p class='light'>Bonobo dapat menggunakan keseluruhan informasi / data Pengguna untuk kebutuhan internal Bonobo tentang riset pasar, promosi tentang produk baru, penawaran khusus, maupun informasi lain, dimana Bonobo dapat menghubungi Pengguna melalui email, surat, telepon, fax.</p><br>
-								<p class='light'>Bonobo dapat meminta Pengguna melengkapi survei yang Bonobo gunakan untuk tujuan penelitian atau lainnya, meskipun Pengguna tidak harus menanggapinya.</p><br>
-								<p class='light'>Bonobo dapat menghubungi Pengguna melalui email, surat, telepon, fax, termasuk namun tidak terbatas, untuk membantu dan/atau menyelesaikan proses transaksi jual beli antar Pengguna.</p><br>
-								<p class='light'>Situs Bonobo memilki kemungkinan terhubung dengan situs-situs lain diluar situs Bonobo, dengan demikian Pengguna menyadari dan memahami bahwa Bonobo tidak turut bertanggung jawab terhadap kerahasiaan informasi Pengguna setelah Pengguna mengakses situs-situs tersebut dengan meninggalkan atau berada diluar situs Bonobo.</p><br>
+								<h3 id='penggunaan' class='light title-faq '>Penggunaan Informasi</h3><br>
+								<p>Bonobo dapat menggunakan keseluruhan informasi / data Pengguna sebagai acuan untuk upaya peningkatan produk dan pelayanan.</p>
+								<p>Bonobo dapat mempergunakan dan mengolah Informasi / data Pengguna dengan tujuan untuk menyesuaikan situs Bonobo sesuai dengan minat Pengguna.</p>
+								<p>Bonobo dapat menggunakan keseluruhan informasi / data Pengguna untuk kebutuhan internal Bonobo tentang riset pasar, promosi tentang produk baru, penawaran khusus, maupun informasi lain, dimana Bonobo dapat menghubungi Pengguna melalui email, surat, telepon, fax.</p>
+								<p>Bonobo dapat meminta Pengguna melengkapi survei yang Bonobo gunakan untuk tujuan penelitian atau lainnya, meskipun Pengguna tidak harus menanggapinya.</p>
+								<p>Bonobo dapat menghubungi Pengguna melalui email, surat, telepon, fax, termasuk namun tidak terbatas, untuk membantu dan/atau menyelesaikan proses transaksi jual beli antar Pengguna.</p>
+								<p>Situs Bonobo memilki kemungkinan terhubung dengan situs-situs lain diluar situs Bonobo, dengan demikian Pengguna menyadari dan memahami bahwa Bonobo tidak turut bertanggung jawab terhadap kerahasiaan informasi Pengguna setelah Pengguna mengakses situs-situs tersebut dengan meninggalkan atau berada diluar situs Bonobo.</p>
 						<a href='#top'>kembali ke atas</a><br>
 								________________________________________
-								<h3 id='pengguna' class='light title-faq '>Pengungkapan informasi Pengguna</h3>
-								<p class='light'>Bonobo menjamin tidak ada penjualan, pengalihan, distribusi atau meminjamkan informasi / data pribadi Anda kepada pihak ketiga lain, tanpa terdapat izin dari Anda. Kecuali dalam hal-hal sebagai berikut:</p><br>
+								<h3 id='pengguna' class='light title-faq '>Pengungkapan Informasi Pengguna</h3><br>
+								<p >Bonobo menjamin tidak ada penjualan, pengalihan, distribusi atau meminjamkan informasi / data pribadi Anda kepada pihak ketiga lain, tanpa terdapat izin dari Anda. Kecuali dalam hal-hal sebagai berikut:</p>
+<ol style='margin-left:15px	'>
 								<p class='light'>
-								<ul>
+								
+								
 								<li>Apabila Bonobo secara keseluruhan atau sebagian assetnya diakuisisi atau merger dengan pihak ketiga, maka data pribadi yang dimiliki oleh pihak Bonobo akan menjadi salah satu aset yang dialihkan atau digabung.</li>
 								<li>Apabila Bonobo berkewajiban mengungkapkan dan/atau berbagi data pribadi Pengguna dalam upaya mematuhi kewajiban hukum dan/atau dalam upaya memberlakukan atau menerapkan syarat-syarat penggunaan Bonobo sebagaimana tercantum dalam Syarat dan Ketentuan Layanan Bonobo dan/atau perikatan perikatan lainnya antara Bonobo dengan pihak ketiga; atau untuk melindungi hak, properti, atau keselamatan Bonobo, pelanggan Bonobo, atau pihak lain. Di sini termasuk pertukaran informasi dengan perusahaan dan organisasi lain untuk tujuan perlindungan Bonobo beserta Penggunanya termasuk namun tidak terbatas pada penipuan, kerugian financial atau pengurangan resiko lainnya.</li>
-								</ul>
+								
+								</ol>
 								</p><br>
 						<a href='#top'>kembali ke atas</a><br>
 								
 								________________________________________
-								<h3 id='merek' class='light title-faq '>Merek Dagang Bonobo</h3>
-								<p class='light'>Situs Bonobo berisi materi informasi / data yang sah dan didaftarkan sesuai ketentuan yang berlaku, penyalahgunaan informasi / data yang telah terdaftar secara sah adalah pelanggaran hukum dan dapat dituntut menurut ketentuan perundang-undangan yang berlaku. Materi informasi / data dimaksud tidak terbatas pada trademark, desain, tampilan, antar muka, dan grafis.</p><br>
-								<p class='light'>Nama dan logo \"Bonobo\" telah terdaftar secara resmi di Departemen Hukum dan Hak Asasi Manusia, Direktorat Jenderal Hak Cipta dan Hak Kekayaan Intelektual Republik Indonesia. Pihak lain dilarang oleh undang-undang untuk menggunakan atau mengatas-namakan dengan nama dan / atau logo \"Bonobo\" untuk kepentingan pribadi dan kelompok tertentu tanpa kuasa yang diberikan khusus untuk itu, dengan atau tanpa sepengetahuan Bonobo. Pelanggaran terhadap hal ini akan dikenai pasal pelanggaran hak cipta dan hak kekayaan intelektual.</p><br>
-								<p class='light'>Tindakan hukum akan dilakukan apabila ditemui tindakan percobaan, baik yang disengaja atau tidak disengaja, untuk mengubah, mengakses halaman situs Bonobo secara paksa yang dibuat bukan untuk konsumsi publik, atau merusak situs Bonobo dan / atau perangkat server yang termuat di dalamnya, tanpa izin khusus yang diberikan oleh pengelola resmi dan sah Bonobo.</p><br>
+								<h3 id='merek' class='light title-faq '>Merek Dagang Bonobo</h3><br>
+								<p>Situs Bonobo berisi materi informasi / data yang sah dan didaftarkan sesuai ketentuan yang berlaku, penyalahgunaan informasi / data yang telah terdaftar secara sah adalah pelanggaran hukum dan dapat dituntut menurut ketentuan perundang-undangan yang berlaku. Materi informasi / data dimaksud tidak terbatas pada trademark, desain, tampilan, antar muka, dan grafis.</p>
+								<p>Nama dan logo \"Bonobo\" telah terdaftar secara resmi di Departemen Hukum dan Hak Asasi Manusia, Direktorat Jenderal Hak Cipta dan Hak Kekayaan Intelektual Republik Indonesia. Pihak lain dilarang oleh undang-undang untuk menggunakan atau mengatas-namakan dengan nama dan / atau logo \"Bonobo\" untuk kepentingan pribadi dan kelompok tertentu tanpa kuasa yang diberikan khusus untuk itu, dengan atau tanpa sepengetahuan Bonobo. Pelanggaran terhadap hal ini akan dikenai pasal pelanggaran hak cipta dan hak kekayaan intelektual.</p>
+								<p>Tindakan hukum akan dilakukan apabila ditemui tindakan percobaan, baik yang disengaja atau tidak disengaja, untuk mengubah, mengakses halaman situs Bonobo secara paksa yang dibuat bukan untuk konsumsi publik, atau merusak situs Bonobo dan / atau perangkat server yang termuat di dalamnya, tanpa izin khusus yang diberikan oleh pengelola resmi dan sah Bonobo.</p>
 						<a href='#top'>kembali ke atas</a><br>
 								________________________________________
-								<h3 id='kritik' class='light title-faq '>Kritik dan Saran</h3>
-								<p class='light'>Segala jenis kritik, saran, maupun keperluan lain dapat disampaikan ke cs@bonoboapp.com.</p><br>
-						<a href='#top'>kembali ke atas</a><br>
-								<p class='light'>Pembaruan Terakhir : 19/01/2015 11:30</p><br>
+								<h3 id='kritik' class='light title-faq '>Kritik dan Saran</h3><br>
+								<p >Segala jenis kritik, saran, maupun keperluan lain dapat disampaikan ke cs@bonoboapp.com.</p><br>
+						<a href='#top'>kembali ke atas</a>
+								<p class='light'>Pembaruan Terakhir : 15 September 2015 11:30 GMT+7</p><br>
 
 						</div>
 					</form>
